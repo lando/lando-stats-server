30f7d19b6a9858c2ac3a95e110a64eb33fe30b3f
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-05-24 18:45:23 -0700

    Added metrics.kbox address to cli tester.

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ var shared = require('./shared.js');
 
 var Client = require('./client.js');
 
-var client = new Client();
+var client = new Client(null, 'http://metrics.kbox');
 
 client.create()
 .then(function() {
@@ -14,10 +14,10 @@ client.create()
   return client.report({action: 'two'});
 })
 .then(function() {
-  return client.report({action: 'two'});
+  return client.report({action: 'three'});
 })
 .then(function() {
-  return client.report({action: 'two'});
+  return client.report({action: 'four'});
 })
 .then(function() {
   return client.get()
