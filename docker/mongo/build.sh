3dee42ceabd09ea71ae9b92f7f77744e0ebfc6d7
root <benjamin.cauldwell@gmail.com>
2015-09-25 20:58:32 +0000

    Added production deployment.

--- docker/mongo/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build --tag kalabox/stats-mongo:v2 ./
-docker push kalabox/stats-mongo:v2
--- docker/node/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build --tag kalabox/stats-node:v2 ./
-docker push kalabox/stats-node:v2
