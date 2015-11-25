2bb46a734bd661dcd0b45ef18388bba32ae85811
root <benjamin.cauldwell@gmail.com>
2015-11-25 23:38:32 +0000

    Rolling out daily stats report.

---
+++ cron-run-report-daily.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ubuntu/data/stats/docker/services/report-daily/
+/usr/local/bin/docker-compose up
---
+++ cron-run-report-weekly.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ubuntu/data/stats/docker/services/report-weekly/
+/usr/local/bin/docker-compose up
